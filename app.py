a6c0958f10916e3cbbc43238fc1d13565156a39a
karen matsunaga <Karen.f.matsunaga@gmail.com>
2022-05-27 21:04:24 +0000

    Atualiza para uma aplicação web usando Flask

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+# Ao abrir o GitPod, execute:
+# pip install -r requirements.txt
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello World'
+
+app.run()
+
 print('Olá mundo')
