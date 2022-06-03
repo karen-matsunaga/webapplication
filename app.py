ca0304b27fbbd754ec20c9ec2eb3d6546971a5f2
karen matsunaga <Karen.f.matsunaga@gmail.com>
2022-06-03 21:02:07 +0000

    Aprimorando o projeto

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 # Ao abrir o GitPod, execute:
 # pip install -r requirements.txt
 
-from flask import Flask
+from flask import Flask, render_templates
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World'
-
-app.run()
+    return 'Hello World' render_templates('index.html', clientes=clientes)
+app.run(debug=True)
 
 print('Olá mundo')
