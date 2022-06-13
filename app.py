822d56775bf6dfe47978c82964a83e5d5933e05b
karen matsunaga <Karen.f.matsunaga@gmail.com>
2022-06-13 16:35:38 +0000

    Web application version 1

--- app.py
+++ app.py
@@ -1,21 +1,88 @@
-# Ao abrir o GitPod, execute:
-# pip install -r requirements.txt
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import redirect
+import csv
+import json
 
-from flask import Flask, render_template
+app = Flask('__name__')
 
-app = Flask(__name__)
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    if request.method == 'GET':
+        fields = json.loads(request.args.get('fields').replace("'", '"'))
+        return render_template('create.html', fields=fields)
+    elif request.method == 'POST':
+        data = dict(request.form)
 
-clientes = [
-
-    {'nome': 'TESTE', 'comentario': 'OI'}
-]
+        with open('computador.csv', 'a') as mais:
+            writer = csv.DictWriter(mais, fieldnames=data.keys())
+            writer.writerow(data)
+        return redirect('/')
 
 @app.route('/')
-def index():
-    return render_template('index.html', clientes=clientes)
+def read():
+    data = []
 
-@app.route('/create')
-def create():
-    return render_template('create.html')
+    with open('computador.csv') as ler:
+        reader = csv.DictReader(ler)
+        for row in reader:
+            data.append(dict(row))
+    return render_template('index.html', data=data, list=list, len=len, str=str)
+
+@app.route('/delete')
+def delete():
+    with open('computador.csv') as deletar:
+        data = []
+        temp_data = []
+        reader = csv.DictReader(deletar)
+
+        for row in reader:
+            temp_data.append(dict(row))
+        [
+            data.append(temp_data[row])
+            for row in range(0, len(temp_data))
+            if row != int(request.args.get('id'))
+        ]
+
+        with open('computador.csv', 'w') as editar:
+            writer = csv.DictWriter(editar, fieldnames=data[0].keys())
+            writer.writeheader()
+            writer.writerows(data)
+    return redirect('/')
+
+@app.route('/update', methods=['GET', 'POST'])
+def update():
+    if request.method == 'GET':
+        data = []
+
+        with open('computador.csv') as atualizar:
+            reader = csv.DictReader(atualizar)
+            for row in reader:
+                data.append(dict(row))
+
+            return render_template('update.html', fields=data[int(request.args.get('id'))])
+
+    elif request.method == 'POST':
+        data = []
+
+    with open('computador.csv') as identificar:
+        reader = csv.DictReader(identificar)
+        for row in reader:
+            data.append(dict(row))
+        row = {}
+
+        for key, val in dict(request.form).items():
+            if key != 'Id':
+                row[key] = val
+
+        data[int(request.form.get('Id'))] = row
+
+        with open('computador.csv', 'w') as edicao:
+            writer = csv.DictWriter(edicao, fieldnames=data[0].keys())
+            writer.writeheader()
+            writer.writerows(data)
+
+        return redirect('/')
 
 app.run(debug=True)
